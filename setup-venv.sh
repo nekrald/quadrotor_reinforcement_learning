2fdab52a790891264ec78513a3c0a6aeca4403f2
Aliaksandr Nekrashevich <nekrald@gmail.com>
2018-04-24 23:34:08 +0300

    Added virtualenv for new libraries.

---
+++ setup-venv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+export PYTHONPATH=libraries/rllab:$PYTHONPATH
+
+set -uexo pipefail
+
+pip3 install virtualenv --upgrade --user
+rm -rf quad-ve
+virtualenv --python python3 quad-ve
+set +uexo pipefail
+. quad-ve/bin/activate
+set -uexo pipefail
+
+pip3 install numpy scipy sklearn
+
+cd libraries/rllab
+python3 setup.py build
+python3 setup.py install
+cd ../..
+
+cd libraries/baseline
+pip3 install -e .
+cd ../..
+
+pip3 install tensorforce[tf]
+pip3 install keras-rl h5py
+
+deactivate
