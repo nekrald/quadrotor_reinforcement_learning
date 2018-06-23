47c0df94890ccb3c92e10de7e9e2573f55f42db2
Aliaksandr Nekrashevich <nekrald@gmail.com>
2018-06-23 17:54:09 -0400

    Introduced requests.

---
+++ code/reinforce/request.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from client import AirSimImageType
+
+
+class RequestType(Enum):
+    VELOCITY = 1
+    POSITION = 2
+    IMAGE    = 3
+
+
+class IRequest(object):
+    def __init__(self, request_type):
+        self.request_type = request_type
+
+
+class VelocityRequest(IRequest):
+    def __init__(self):
+        super(VelocityRequest, self).__init__(RequestType.VELOCITY)
+
+
+class PositionRequest(IRequest):
+    def __init__(self):
+        super(PositionRequest, self).__init__(RequestType.POSITION)
+
+
+class ImageRequest(IRequest):
+    def __init__(self, image_type: AirSimImageType):
+        super(ImageRequest, self).__init__(RequestType.IMAGE)
+        self.image_type = image_type
+
